b1345c758ff6c71ad72e708a643948de74f031ae
Ryan Luu <rluu@purdue.edu>
2021-02-11 01:21:37 -0500

    Scripting

--- scripts/emap2sec.sh
+++ scripts/emap2sec.sh
@@ -1,9 +1,21 @@
 USER="rluu"
 REMOTE="brown.rcac.purdue.edu"
 IDENTITY="~/.ssh/id_rsa"
+WD="/home/rluu/emap2sec"
 
-remote_command() {
-    ssh -i $IDENTITY $USER@$REMOTE "$1"
+remote_cp() {
+    scp -i $IDENTITY $1 $USER@$REMOTE:$WD/$2
 }
 
-remote_command "touch $1"
+remote_sh() {
+    ssh -i $IDENTITY $USER@$REMOTE "cd $WD; $1"
+}
+
+params=cat $1
+
+get_param() {
+    $params
+}
+
+echo $(get_param id)
+
